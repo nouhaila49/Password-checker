f2bfac4ae8ab2d751faa4490db7e4bc79c18e664
Nouhaila <57545080+nouhaila49@users.noreply.github.com>
2025-06-23 14:57:14 +0200

    Delete zxcvbn_imple.py

--- zxcvbn_imple.py
+++
@@ -1,9 +0,0 @@
-from zxcvbn import zxcvbn
-
-def zxcvbn_check(password ):        
-
-    result = zxcvbn(password)
-
-    print("Password Strength Score:", result['score'])
-
-    print("Feedback:", result['feedback'])
